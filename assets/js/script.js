1a055d63ff995e28288d01d2ff0dc185153bf643
rafikadir <rafikadir2000@gmail.com>
2022-07-22 02:36:31 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// Ordinary JS
+function displayImg(){
+    const ordinaryImg = document.getElementById("ordinary-img");
+    ordinaryImg.style.display = "block";
+    const ordinaryVideo = document.getElementById("ordinary-video");
+    ordinaryVideo.style.display = "none";
+}
+
+function displayVideo(){
+    const ordinaryImg = document.getElementById("ordinary-img");
+    ordinaryImg.style.display = "none";
+    const ordinaryVideo = document.getElementById("ordinary-video");
+    ordinaryVideo.style.display = "block";
+}
+
